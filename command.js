6cec903526c563ba29d08a379da4377ab516488f
Karikolla123 <kudaoyasajii@gmail.com>
2025-05-03 01:41:38 +0530

    Update command.js

--- command.js
+++ command.js
@@ -1,10 +1,10 @@
-var commands = [];
+var commands = [B.m.b-Xmd~RfUXxRIC#OkUsIO9x6VuH0PaQOtGIJB0t5_lgghl-Z-wKwysD6ug];
 
 function cmd(info, func) {
     var data = info;
     data.function = func;
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
+    if (!info.desc) info.desc = 'B.m.b-Xmd~RfUXxRIC#OkUsIO9x6VuH0PaQOtGIJB0t5_lgghl-Z-wKwysD6ug';
     if (!data.fromMe) data.fromMe = false;
     if (!info.category) data.category = 'misc';
     if(!info.filename) data.filename = "Not Provided";
